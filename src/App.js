fd0db52e34f99bec0487e00e7b3f58244745eaa8
echinoidea <gabriel.i.hooks@gmail.com>
2024-07-26 01:27:42 -0400

    use express backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import './App.css';
 
-import { createClient } from '@supabase/supabase-js'
 import { useState, useEffect } from 'react';
 import DataTable from './DataTable';
-
-const supabaseUrl = 'https://rclumlmyqtxgmthpqsrj.supabase.co'
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-// Get all rows from table given table name
-const fetchAllRows = async (tableName) => {
-  const { data, error } = await supabase
-    .from(tableName)
-    .select()
-
-  if (error) {
-    console.error('Error fetching data:', error)
-    return []
-  }
-
-  return data
-}
 
 function App() {
   const [ classData, setClassData ] = useState([]);
@@ -28,12 +9,12 @@ function App() {
   // Get class table data on load
   useEffect(() => {
     const getData = async () => {
-      const tableData = await fetchAllRows('class')
-      setClassData(tableData)
+      await fetch('http://localhost:8000/api/class')
+      .then(response => response.json())
+      .then(data => setClassData(data));
     }
-    
     getData()
-  }, [])
+  }, []);
 
   return (
     <div className="App">
